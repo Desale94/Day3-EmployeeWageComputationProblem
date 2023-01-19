aab4d1f390ca7a32890f40418438449d7415c518
Desale1 <dkdesale08@gmail.com>
2023-01-19 13:05:30 +0530

    check employee is present or absent

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,5 +3,12 @@ package com.bridgelabz.employee;
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation problem!!");
+        int Is_Full_Time = 1;
+        //constant
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == Is_Full_Time)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
