d2956efad8263456e7cc8d21655ee77a543e37e7
Desale1 <dkdesale08@gmail.com>
2023-01-19 13:22:53 +0530

    Calculating Wages for a Month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,25 +3,31 @@ package com.bridgelabz.employee;
 public class EmpWageBuilder {
     public static final int Is_Part_Time = 1;
     public static final int Is_Full_Time = 2;
-    public static final int EMP_RATE_PER_HOUR= 20;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+
     public static void main(String[] args) {
-        // System.out.println("Welcome to EmployeeWage Computation Program");
+
         //variables
-        int empHrs=0;
-        int empWage=0;
-        int empCheck =(int) Math.floor(Math.random()*10)%3;
-        switch (empCheck){
-            case Is_Part_Time:
-                empHrs=4;
-                break;
-            case Is_Full_Time:
-                empHrs=8;
-                break;
-            default:
-                empHrs=0;
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        //computation
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case Is_Part_Time:
+                    empHrs = 4;
+                    break;
+                case Is_Full_Time:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs + EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Emp Wage: " + empWage);
+
         }
-        empWage= empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage: " + empWage);
-        //System.out.println("Employe is Absent");
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
